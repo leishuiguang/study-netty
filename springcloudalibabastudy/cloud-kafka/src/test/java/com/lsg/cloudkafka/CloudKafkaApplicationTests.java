8ab4a30ef0638cb006b62ad078aede8fe6c8245d
lsg <leishuiguang@sina.com>
2021-04-28 19:30:22 +0800

    新增模块

---
+++ springcloudalibabastudy/cloud-kafka/src/test/java/com/lsg/cloudkafka/CloudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsg.cloudkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
